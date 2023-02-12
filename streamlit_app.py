286f8919bc883ba175e626fdd873ae75c9f26906
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-12 05:21:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,20 @@
 import openai
 import json
 import streamlit as st
-import os
 
-# Autenticación de OpenAI (oculta la clave en una variable de entorno)
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+# Autenticación en OpenAI
+openai.api_key = "YOUR_API_KEY_HERE"
 
 # Configuración del modelo GPT-3
 model_engine = "text-davinci-003"
 
 # Función para generar el correo
-def generate_email(name, company, role, linkedin_url):
+def generate_email(name, company, role, linkedin_url, proposal):
     prompt = (f"Hola {name},\n\n" 
               "He estado investigando tu perfil en LinkedIn y me ha impresionado mucho tu experiencia en {role} en {company}. "
               "Me gustaría saber si estarías interesado en hablar sobre una posible colaboración en un proyecto relacionado con {role} en nuestra empresa. "
               "Permíteme proporcionarte más detalles sobre la propuesta de negocio:\n\n"
-              "Nuestra empresa se dedica a [descripción de la empresa]. Actualmente estamos trabajando en un proyecto relacionado con [descripción del proyecto]."
-              "Creemos que tu experiencia y habilidades podrían ser de gran valor para el proyecto, y nos gustaría discutir una posible colaboración contigo. "
-              "Por favor, háznos saber si estarías interesado en saber más sobre el proyecto y cómo podrías contribuir.\n\n"
+              f"{proposal}\n\n"
               "Te he enviado una solicitud de conexión en LinkedIn para que podamos mantenernos en contacto.\n\n"
               "Saludos cordiales,\n"
               "Tu nombre")
@@ -39,8 +36,9 @@ name = st.text_input("Ingresa el nombre de la persona:")
 company = st.text_input("Ingresa la empresa en la que trabaja la persona:")
 role = st.text_input("Ingresa el puesto de la persona:")
 linkedin_url = st.text_input("Ingresa la URL del perfil de LinkedIn de la persona:")
+proposal = st.text_area("Ingresa la propuesta de negocio:")
 
 if st.button("Generar correo"):
     # Generar el correo y mostrar el resultado en la interfaz de usuario
-    email = generate_email(name, company, role, linkedin_url)
+    email = generate_email(name, company, role, linkedin_url, proposal)
     st.text_area("Correo generado:", value=email, height=200, max_chars=None)
