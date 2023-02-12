42b7e50593843171421c28d1e00366be16b0edb1
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-12 09:40:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,15 @@ openai.api_key = "YOUR_API_KEY"
 st.title("Generador de Cartas de Negocios") 
 st.header("Proporciona la URL del perfil de LinkedIn y el nombre de tu propuesta") 
 
- # Obtener información del usuario y almacenarla en variables  
+# Obtener información del usuario y almacenarla en variables  
 linkedInURL = st.text_input("URL del perfil de LinkedIn:")  
 proposalName = st.text_input("Nombre de tu propuesta:")  
 
  # Generar carta usando GPT-3 y la información proporcionada por el usuario   
 prompt = f"Hola, estoy escribiendo para presentarte mi idea llamada '{proposalName}'. Me enteré que {linkedInURL} es uno de tus intereses principales, y me gustaría compartir contigo cómo puedo ayudarte en este área."    
 
-completion = openai.Completion(engine="davinci", prompt=prompt, max_tokens=150)    							    
-response = completion.get()['choices'][0]['text'] 
+completion = openai.Completion(engine="davinci", prompt=prompt, max_tokens=150)    
+response = completion.get(prompt)['choices'][0]['text'] 
 
 #Mostrar carta generada en pantalla    
 st.write(response)
